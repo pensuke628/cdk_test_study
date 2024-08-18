8565e4c275d8ba7ed03e4dcbc63d877c41ec2827
pensuke628 <ryz123212327@gmail.com>
2024-08-18 22:18:26 +0900

    feat: s3とassertionテストを追加

--- lib/cdk_test_study-stack.ts
+++ lib/cdk_test_study-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class CdkTestStudyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +17,23 @@ export class CdkTestStudyStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED
         }
       ]
+    });
+
+    const Bucket = new s3.Bucket(this, 'CdkTestStudyBucket1', {
+      bucketName: 'hogehoge',
+      enforceSSL: true,
+      objectLockEnabled: true,
+      versioned: true,
+    });
+
+    new s3.Bucket(this, 'CdkTestStudyBucket2', {
+      objectLockEnabled: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN
+    });
+
+    // S3のBucketNameをoutput
+    new cdk.CfnOutput(this, 'S3BucketName', {
+      value: Bucket.bucketName,
     });
   }
 }
